7dedb1dc7bc0297ea6efbc0baf10c85c8146cca7
Liam Gladdy <liam@gladdy.co.uk>
2014-02-12 21:08:54 +0000

    Add docs

--- culture-object-sync/providers/culturegrid.class.php
+++ culture-object-sync/providers/culturegrid.class.php
@@ -3,7 +3,11 @@
 class Culture_Object_Sync_Provider_CultureGrid_Exception extends Culture_Object_Sync_Provider_Exception { }
 
 class Culture_Object_Sync_Provider_CultureGrid extends Culture_Object_Sync_Provider {
+  //http://www.culturegrid.org.uk/index/select?fl=*&wt=json&rows=1000000&indent=on&q=(authority:ManchesterCityGalleries)%20AND%20(dcmi.type:PhysicalObject)&start=0
   
+  //http://www.culturegrid.org.uk/index/select?wt=json&rows=5000&indent=on&q=dcmi.type:PhysicalObject&start=0
+  
+  //http://www.culturegrid.org.uk/wp-content/uploads/2011/11/Culture-Grid-search-service-v3.pdf
 }
 
 ?>
