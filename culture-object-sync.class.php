054069ce58abb0a95dfde4f724d503266408ef61
Liam Gladdy <liam@gladdy.co.uk>
2014-02-13 22:34:22 +0000

    COS settings

--- culture-object-sync.class.php
+++ culture-object-sync.class.php
@@ -4,7 +4,6 @@ require_once('culture-object-sync/core.class.php');
 require_once('culture-object-sync/exceptions.class.php');
 require_once('culture-object-sync/provider.class.php');
 require_once('culture-object-sync/settings.class.php');
-require_once('culture-object-sync/providers/culturegrid.class.php');
 
 class Culture_Object_Sync extends Culture_Object_Sync_Core {
   
