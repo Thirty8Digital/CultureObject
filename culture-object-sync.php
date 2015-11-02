4c870c5f7de2558eeadfa2113cdd6e884396e3be
Liam Gladdy <liam@gladdy.co.uk>
2015-11-02 20:09:57 +0000

    Add 'cos_get_field' and 'cos_the_field' to mimic ACF's get_field and the_field

--- culture-object-sync.php
+++ culture-object-sync.php
@@ -21,3 +21,13 @@ function cos_remapped_field_name($field_key) {
 	global $cos;
 	return $cos->helper->cos_remapped_field_name($field_key);
 }
+
+function cos_get_field($field_key) {
+	$id = get_the_ID();
+	if (!$id) return false;
+	echo get_post_meta($id, $field_key, true);
+}
+
+function cos_the_field($field_key) {
+	echo cos_get_field($field_key);
+}
