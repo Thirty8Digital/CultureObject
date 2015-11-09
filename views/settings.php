1dbb74bb7f774c323a8e4527003a6fcb0ce4179b
Liam Gladdy <liam@gladdy.co.uk>
2015-11-09 16:58:26 +0000

    Rename plugin to just Culture Object

--- views/settings.php
+++ views/settings.php
@@ -6,8 +6,8 @@
     <?php delete_transient('cos_message'); ?>
   <?php } ?>
 
-  <?php $plugin_data = get_plugin_data(__DIR__.'/../culture-object-sync.php'); ?>
-  <h2>Culture Object Sync Settings <small>Version <?php echo $plugin_data['Version']; ?> by <a href="http://www.thirty8.co.uk">Thirty8 Digital</a>.</small></h2>
+  <?php $plugin_data = get_plugin_data(__DIR__.'/../culture-object.php'); ?>
+  <h2>Culture Object Settings <small>Version <?php echo $plugin_data['Version']; ?> by <a href="http://www.thirty8.co.uk">Thirty8 Digital</a>.</small></h2>
 
   <?php
   $key = get_option('cos_core_sync_key');
@@ -22,7 +22,7 @@
     <p><strong>Your current provider, <?php echo $provider_info['name']; ?>, doesn't support automated import by cron. You do not need to create a cronjob for this provider.</strong></p>
   <?php } else { ?>
   
-    <p>The Culture Object Sync plugin requires you to set up a manual cronjob to the frequency you wish to check for updates for your chosen sync provider.</p>
+    <p>The Culture Object plugin requires you to set up a manual cronjob to the frequency you wish to check for updates for your chosen sync provider.</p>
     <p>You shouldn't do this too frequently to avoid causing problems with your provider. Once a day should be enough.</p>
     <p>You should load the following URL:<br /><?php echo get_site_url(); ?>?perform_culture_object_sync=true&key=<?php echo get_option('cos_core_sync_key'); ?></p>
 
