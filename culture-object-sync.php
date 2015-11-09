75f25c36911f5fed82357f21807d096304ad829b
Liam Gladdy <liam@gladdy.co.uk>
2015-11-09 16:52:02 +0000

    Match github tag versioning

--- culture-object-sync.php
+++ culture-object-sync.php
@@ -3,7 +3,7 @@
  * Plugin Name: Culture Object Sync
  * Plugin URI: http://www.gladdy.co.uk/projects/culture-object-sync
  * Description: A framework as a plugin to enable sync of culture objects into WordPress.
- * Version: 2.0
+ * Version: 2.0.0
  * Author: Liam Gladdy / Thirty8 Digital
  * Author URI: https://www.gladdy.uk / http://www.thirty8digital.co.uk
  * License: Apache 2 License
