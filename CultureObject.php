f36a5f5be873593cf4aca0dc75ab6332d976fea9
Liam Gladdy <liam@gladdy.co.uk>
2016-06-19 19:50:56 +0100

    Make activation hooks work

--- CultureObject.php
+++ CultureObject.php
@@ -13,9 +13,43 @@
  */
 
 require_once('CultureObject/CultureObject.class.php');
+
+register_activation_hook(__FILE__, 'regenerate_permalinks');
+register_activation_hook(__FILE__, 'check_versions');
+register_deactivation_hook(__FILE__, 'regenerate_permalinks');
+
 $cos = new \CultureObject\CultureObject();
 
 /* General Functions. These need to go into their own file one day. */
+
+function check_versions() {
+    global $wp_version;
+    $wp = '4.5';
+    $php = '5.5';
+    
+    if (version_compare(PHP_VERSION, $php, '<')) {
+        $flag = 'PHP';
+    } elseif (version_compare($wp_version, $wp, '<')) {
+        $flag = 'WordPress';
+    } else return;
+    $version = 'PHP' == $flag ? $php : $wp;
+    deactivate_plugins(basename( __FILE__ ));
+    
+    $error_type = __('Plugin Activation Error', 'culture-object');
+    $error_string = sprintf(
+        /* Translators: 1: Either WordPress or PHP, depending on the version mismatch 2: Required version number */
+        __('Culture Object requires %1$s version %2$s or greater.', 'culture-object'),
+        $flag,
+        $version
+    );
+    
+    wp_die('<p>'.$error_string.'</p>', $error_type,  array('response'=>200, 'back_link'=>TRUE));
+}
+
+    
+function regenerate_permalinks() {
+    flush_rewrite_rules();
+}
 
 function cos_get_remapped_field_name($field_key) {
     global $cos;
