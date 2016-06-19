875041171ad09acdf092f598ce8a2947a2f7b821
Liam Gladdy <liam@gladdy.co.uk>
2016-06-19 20:06:26 +0100

    Properly fix activation hook check

--- CultureObject/CultureObject.class.php
+++ CultureObject/CultureObject.class.php
@@ -25,7 +25,36 @@ class CultureObject extends Core {
     function load_co_languages() {
         load_plugin_textdomain('culture-object', FALSE, basename(dirname( __FILE__ )).'/languages/');
     }
+    
+    static function check_versions() {
+        global $wp_version;
+        $wp = '4.5';
+        $php = '5.5';
         
+        if (version_compare(PHP_VERSION, $php, '<')) {
+            $flag = 'PHP';
+        } elseif (version_compare($wp_version, $wp, '<')) {
+            $flag = 'WordPress';
+        } else return;
+        $version = 'PHP' == $flag ? $php : $wp;
+        deactivate_plugins(basename( __FILE__ ));
+        
+        $error_type = __('Plugin Activation Error', 'culture-object');
+        $error_string = sprintf(
+            /* Translators: 1: Either WordPress or PHP, depending on the version mismatch 2: Required version number */
+            __('Culture Object requires %1$s version %2$s or greater.', 'culture-object'),
+            $flag,
+            $version
+        );
+        
+        wp_die('<p>'.$error_string.'</p>', $error_type,  array('response'=>200, 'back_link'=>TRUE));
+    }
+
+    
+    static function regenerate_permalinks() {
+        flush_rewrite_rules();
+    }
+    
     function should_sync() {
         
         if (isset($_GET['perform_culture_object_sync']) && isset($_GET['key'])) {
